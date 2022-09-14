ff0528b96302f1ed76960186d1d9990f095f14a0
Swislok-Dev <cmarker4@gmail.com>
2022-09-14 08:11:56 -0500

    add header to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Home from './pages/Home.jsx'
+import Header from "./components/Header.jsx"
+
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Home />
     </div>
   );
