41f36be6b9487e367aa128abc30ef109e5f9e3e4
Swislok-Dev <cmarker4@gmail.com>
2022-09-14 08:12:16 -0500

    add sample photos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react"
+import {img1, img2, img3, img4, img5, img6, coolGalaxy, blackHeart} from "../assets/photos/index.js"
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       <h1>Home Component</h1>
+      <figure>
+        <img src={img1} alt="img1" />
+        <img src={img2} alt="img2" />
+        <img src={img3} alt="img3" />
+        <img src={img4} alt="img4" />
+        <img src={img5} alt="img5" />
+        <img src={img6} alt="img6" />
+        <img src={coolGalaxy} alt="cool galaxy" />
+        <img src={blackHeart} alt="black heart logo" />
+      </figure>
     </div>
   )
 }
