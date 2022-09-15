4ec3c7becc467e338bcde9c2212e74be83535c0c
Swislok-Dev <cmarker4@gmail.com>
2022-09-15 11:40:04 -0500

    add assets

---
+++ src/assets/photos/index.js
@@ -0,0 +1,9 @@
+export { default as img1 } from "./img1.JPG"
+export { default as img2 } from "./img2.jpg"
+export { default as img3 } from "./img3.jpeg"
+export { default as img4 } from "./img4.JPG"
+export { default as img5 } from "./img5.JPG"
+export { default as img6 } from "./img6.JPG"
+export {default as coolGalaxy} from "./cool_galaxy_pic.jpeg"
+export {default as blackHeart} from "./BLKHRT-Cover.jpeg"
+
