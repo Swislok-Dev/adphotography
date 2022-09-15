eead4886325ca80f66b850900821c536cad10e08
Swislok-Dev <cmarker4@gmail.com>
2022-09-15 11:36:24 -0500

    scrollable navlinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,53 +15,86 @@ function Navbar() {
     setIsActive(false)
   }
 
-  return (
-    <nav className="navbar">
-      <ul
-        onClick={removeHamburgerActive}
-        className={`nav-menu ${showActiveClass()}`}
-      >
-        <li className="nav-item">
-          <a href="#1" className="nav-link">
-            Family
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#2" className="nav-link">
-            Couples
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#3" className="nav-link">
-            Baby
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#3" className="nav-link">
-            Wedding
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#3" className="nav-link">
-            Art Prints
-          </a>
-        </li>
-        <li className="nav-item">
-          <a href="#3" className="nav-link">
-            Contact
-          </a>
-        </li>
-      </ul>
+  // className => "img9" is last image
 
-      <div
-        onClick={onHamburgerActive}
-        className={`hamburger ${showActiveClass()}`}
-      >
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-      </div>
-    </nav>
+  const scrollTo = (scrollId) => {
+    const anchor = document.querySelector(`#${scrollId}`)
+    anchor.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    })
+  }
+
+  return (
+    <div>
+      <nav className="navbar">
+        <ul
+          onClick={removeHamburgerActive}
+          className={`nav-menu ${showActiveClass()}`}
+        >
+          <li className="nav-item">
+            <a
+              onClick={() => scrollTo("family")}
+              href="#1"
+              className="nav-link"
+            >
+              Family
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              onClick={() => scrollTo("couples")}
+              href="#2"
+              className="nav-link"
+            >
+              Couples
+            </a>
+          </li>
+          <li className="nav-item">
+            <a onClick={() => scrollTo("baby")} href="#3" className="nav-link">
+              Baby
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              onClick={() => scrollTo("wedding")}
+              href="#3"
+              className="nav-link"
+            >
+              Wedding
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              onClick={() => scrollTo("art-prints")}
+              href="#3"
+              className="nav-link"
+            >
+              Art Prints
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              onClick={() => scrollTo("contact")}
+              href="#3"
+              className="nav-link"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        <div
+          onClick={onHamburgerActive}
+          className={`hamburger ${showActiveClass()}`}
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </nav>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,32 @@
 import React from "react"
-import {img1, img2, img3, img4, img5, img6, coolGalaxy, blackHeart} from "../assets/photos/index.js"
+import {
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  coolGalaxy,
+  blackHeart,
+} from "../assets/photos/index.js"
 
 function Home() {
   return (
     <div className="home">
-      <figure>
-        <img src={img1} alt="img1" />
-        <img src={img2} alt="img2" />
-        <img src={img3} alt="img3" />
-        <img src={img3} alt="img3" />
-        <img src={img4} alt="img4" />
-        <img src={img5} alt="img5" />
-        <img src={img6} alt="img6" />
-        <img src={coolGalaxy} alt="cool galaxy" />
-        <img src={blackHeart} alt="black heart logo" />
+      <figure id="family">
+        <img id="img1" src={img1} alt="img1" />
+        <img id="img2" src={img2} alt="img2" />
+        <img id="img3" src={img3} alt="img3" />
+      </figure>
+      <figure id="couples">
+        <img id="img4" src={img3} alt="img3" />
+        <img id="img5" src={img4} alt="img4" />
+        <img id="img6" src={img5} alt="img5" />
+      </figure>
+      <figure id="baby">
+        <img id="img7" src={img6} alt="img6" />
+        <img id="img8" src={coolGalaxy} alt="cool galaxy" />
+        <img id="img9" src={blackHeart} alt="black heart logo" />
       </figure>
     </div>
   )
