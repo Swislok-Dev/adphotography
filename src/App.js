3cba882dec96bec19b564302b3556eb0d6c8ee02
Swislok-Dev <cmarker4@gmail.com>
2022-09-21 09:52:47 -0500

    implement scroll to top on page reload

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react"
 import "./App.css"
 import Header from "./components/Header.jsx"
 import Home from "./pages/Home.jsx"
+import ScrollToTop from "./components/ScrollToTop"
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTop />
       <React.Suspense fallback={<div>Loading...</div>}>
         <Header />
         <Home />
---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function ScrollToTop() {
+  React.useEffect(() => {
+    window.onbeforeunload = () => {
+      window.scrollTo(0, 0)
+    }
+  })
+
+  return null
+}
+
+export default ScrollToTop
