00641b63a8aa020bcd21e176c7a3855f15df4ecf
Swislok-Dev <cmarker4@gmail.com>
2022-09-27 15:31:05 -0500

    write new show component

--- src/components/ShowImage.jsx
+++ src/components/ShowImage.jsx
@@ -1,25 +1,23 @@
 import React from "react"
-import {LazyLoadImage} from "react-lazy-load-image-component"
+import "lazysizes"
 
-
-const ShowImage = ({imagesPath}) => {
-  const [babypics, setBabypics] = React.useState([])
-
-  const importAll = (req) => {
-    return req.keys().map(req)
-  }
-
-
-  React.useEffect(() => {
-    const babyphotos = importAll(
-      require.context("../assets/photos/baby", false, /\.(png|JPE?G|jpe?g)$/)
-    )
-    return setBabypics(babyphotos)
-  }, [])
-
-  return <>
-    {babypics.map((item, index) => <LazyLoadImage key={item[index]} src={item} alt="babypic" />)}
-  </>
+const ShowImage = ({ imagesPath }) => {
+  return (
+    <>
+      {imagesPath
+        ? imagesPath.map((res) => {
+            return (
+              <img
+                className="lazyload"
+                data-src={res.url}
+                alt={res.id}
+                key={res.id}
+              />
+            )
+          })
+        : null}
+    </>
+  )
 }
 
 export default ShowImage
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,27 @@
 import React from "react"
-import "lazysizes"
-import * as pics from "../assets/photos/index"
+import ShowImage from "../components/ShowImage"
+import images from "../data/images.json"
 
 function Home() {
   return (
     <div className="home">
       <figure id="family">
-        <img src={pics.img2} alt="img2" className="lazyload" />
-        <img src={pics.img3} alt="img3" className="lazyload" />
+        <ShowImage imagesPath={images.family} />
       </figure>
 
       <figure id="couples">
-        <img data-src={pics.img5} alt="img5" className="lazyload" />
+        <ShowImage imagesPath={images.couples} />
       </figure>
 
       <figure id="baby">
-        <img data-src={pics.img1} alt="img1" className="lazyload" />
-        <img data-src={pics.img4} alt="img4" className="lazyload" />
-        <img data-src={pics.img6} alt="img6" className="lazyload" />
+        <ShowImage imagesPath={images.baby} />
       </figure>
 
-      <figure id="wedding"></figure>
+      <figure id="wedding">
+        <ShowImage imagesPath={images.wedding} />
+      </figure>
       <figure id="art-prints">
-        <img data-src={pics.bh} alt="bh" className="lazyload" />
+        <ShowImage imagesPath={images.artPrints} />
       </figure>
     </div>
   )
