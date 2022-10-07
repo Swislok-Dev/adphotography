947cb258f4d58c630957b013d9a2e1d07f1b4fb4
Swislok-Dev <cmarker4@gmail.com>
2022-10-07 09:47:19 -0500

    onClick event for header to scroll to top

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,18 @@ import React from "react"
 import Navbar from "./Navbar"
 
 function Header() {
+  const scrollTo = (scrollId) => {
+    const anchor = document.querySelector(`#${scrollId}`)
+    anchor.scrollIntoView({
+      behavior: "smooth",
+    })
+  }
   return (
     <header className="header">
       <div className="header-content">
-        <h3>ANNA DAHLIA PHOTOGRAPHY</h3>
+        <a className="header-link" onClick={() => scrollTo("family")} href="#/">
+          <h3>ANNA DAHLIA PHOTOGRAPHY</h3>
+        </a>
         <Navbar />
       </div>
     </header>
