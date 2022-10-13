e8425b3bf60d5305a00e5e7a8998ab92a2893b33
Swislok-Dev <cmarker4@gmail.com>
2022-10-13 08:34:23 -0500

    export image directories

---
+++ src/data/images.js
@@ -0,0 +1,10 @@
+function importAll(r) {
+  return r.keys().map(r);
+}
+
+export const familyImages =  importAll(require.context('/public/assets/images/family', false, /\.(png|jpe?g|svg)$/));
+export const couplesImages =  importAll(require.context('/public/assets/images/couples', false, /\.(png|jpe?g|svg)$/));
+export const babyImages =  importAll(require.context('/public/assets/images/baby', false, /\.(png|jpe?g|svg)$/));
+export const weddingImages =  importAll(require.context('/public/assets/images/wedding', false, /\.(png|jpe?g|svg)$/));
+export const artPrintImages =  importAll(require.context('/public/assets/images/artPrints', false, /\.(png|jpe?g|svg)$/));
+
