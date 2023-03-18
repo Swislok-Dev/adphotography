6514e80e9121ec7f5627263c8d1e6413109cb289
cm@MBA <cmarker4@gmail.com>
2023-03-18 09:49:35 -0500

    create Layout component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Footer from './Footer';
+import Header from './Header';
+import SEO from './seo';
+
+function Layout({ children }) {
+  return (
+    <>
+      <SEO />
+      <div className="content">
+        <Header />
+        <main>{children}</main>
+        <Footer/>
+      </div>
+    </>
+  );
+}
+
+export default Layout;
