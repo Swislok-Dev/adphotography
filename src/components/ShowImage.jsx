5297324d52aa0d1db393697c3d882e9318165b2c
cm@MBA <cmarker4@gmail.com>
2023-03-21 11:57:49 -0500

    set individual keys to each image

--- src/components/ShowImage.jsx
+++ src/components/ShowImage.jsx
@@ -5,14 +5,14 @@ const ShowImage = ({ imagesPath }) => {
   return (
     <>
       {imagesPath
-        ? imagesPath.map((res) => {
+        ? imagesPath.map((res, index) => {
             return (
               <Image
                 src={res}
                 height={500}
                 width={500}
                 alt={res}
-                key={res}
+                key={res + index}
                 placeholder="blur"
                 sizes="(max-width: 768px) 100vw,
                 (max-width: 1200px) 50vw, 33vw"
