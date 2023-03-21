6e33e2aae2e5356e69a374f5ba05332bf1c9e535
cm@MBA <cmarker4@gmail.com>
2023-03-21 11:35:29 -0500

    update manifest.json for favicon

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -6,12 +6,12 @@ function SEO() {
       <title>Anna Dahlia Photography</title>
       <meta name="description" content="Photography portfolio website" />
       <meta property="og:type" content="website" />
-      <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" />
       <link rel="apple-touch-icon" href="%PUBLIC_URL%/favicon.ico" />
       <meta name="apple-mobile-web-app-title" content="AD Photos" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
-      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+      <link rel="manifest" href="/manifest.json" />
     </Head>
   );
 }
